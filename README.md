852e6794c1f9d00bad5075e81733e2da2efc1516
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-04-03 10:00:28 +0300

    ch

---
+++ README.md
@@ -0,0 +1,3 @@
+Project:
+https://drive.google.com/file/d/1DN5Gpjw6uJZuSvGBdXzwm-SHcBEn0PE-/view
+https://drive.google.com/file/d/1F2cYb2cIPFG0B_GybMcnNUPtc5mq8mHY/view
---
+++ src/main/java/edu/nand2tetris/VMTranslator.java
@@ -0,0 +1,7 @@
+package edu.nand2tetris;
+
+public class VMTranslator {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
