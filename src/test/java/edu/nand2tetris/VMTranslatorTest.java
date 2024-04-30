e1cf626f678ee290b8f7f71fcc9d657b9aae880a
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-05-01 01:33:36 +0500

    add test passed

--- src/test/java/edu/nand2tetris/VMTranslatorTest.java
+++ src/test/java/edu/nand2tetris/VMTranslatorTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 public final class VMTranslatorTest {
     @Test
     public void basicTest() {
-        final Path src = Resources.RESOURCES_DIR.resolve("translator/StaticTest.vm");
-        final Path out = Resources.RESOURCES_DIR.resolve("translator/StaticTest.asm");
+        final Path src = Resources.RESOURCES_DIR.resolve("translator/SimpleAdd.vm");
+        final Path out = Resources.RESOURCES_DIR.resolve("translator/SimpleAdd.asm");
         VMTranslator.main(new String[]{src.toString(), out.toString()});
     }
 }
