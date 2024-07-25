d1514465f0aa8e9d7dec2be53e2b7fa40ad5997f
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-07-26 02:01:29 +0300

    refactored

--- src/test/java/edu/nand2tetris/VMTranslatorTest.java
+++ src/test/java/edu/nand2tetris/VMTranslatorTest.java
@@ -14,110 +14,77 @@ import nand2tetris.test.lib.ASMTranslationException;
 public final class VMTranslatorTest {
     private final static Pattern VM_FILE_NAME = Pattern.compile("^\\S+.vm$");
 
+    private void runTestWithFile(Path dir, String asmFileName, String tstFileName) throws IOException, ASMTranslationException {
+        final Path file = getVmFiles(dir).next();
+        VMTranslator.main(new String[]{file.toAbsolutePath().toString(), dir.resolve(asmFileName).toString()});
+        ASMTester.executeTestScript(dir.resolve(tstFileName));
+    }
+
+    private void runTestWithDir(Path dir, String asmFileName, String tstFileName) throws IOException, ASMTranslationException {
+        VMTranslator.main(new String[]{dir.toAbsolutePath().toString(), dir.resolve(asmFileName).toString()});
+        ASMTester.executeTestScript(dir.resolve(tstFileName));
+    }
+
     @Test
     public void testMemoryAccessBasicTest() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("MemoryAccess").resolve("BasicTest");
-        final Path file = getVmFiles(basicTestDir).next();
-
-        VMTranslator.main(new String[]{file.toAbsolutePath().toString(), basicTestDir.resolve("BasicTest.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("BasicTest.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("MemoryAccess").resolve("BasicTest");
+        runTestWithFile(dir, "BasicTest.asm", "BasicTest.tst");
     }
 
     @Test
     public void testMemoryAccessPointerTest() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("MemoryAccess").resolve("PointerTest");
-        final Path file = getVmFiles(basicTestDir).next();
-
-        VMTranslator.main(new String[]{file.toAbsolutePath().toString(), basicTestDir.resolve("PointerTest.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("PointerTest.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("MemoryAccess").resolve("PointerTest");
+        runTestWithFile(dir, "PointerTest.asm", "PointerTest.tst");
     }
 
     @Test
     public void testMemoryAccessStaticTest() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("MemoryAccess").resolve("StaticTest");
-        final Path file = getVmFiles(basicTestDir).next();
-
-        VMTranslator.main(new String[]{file.toAbsolutePath().toString(), basicTestDir.resolve("StaticTest.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("StaticTest.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("MemoryAccess").resolve("StaticTest");
+        runTestWithFile(dir, "StaticTest.asm", "StaticTest.tst");
     }
 
     @Test
     public void testStackArithmeticSimpleAdd() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("StackArithmetic").resolve("SimpleAdd");
-        final Path file = getVmFiles(basicTestDir).next();
-
-        VMTranslator.main(new String[]{file.toAbsolutePath().toString(), basicTestDir.resolve("SimpleAdd.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("SimpleAdd.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("StackArithmetic").resolve("SimpleAdd");
+        runTestWithFile(dir, "SimpleAdd.asm", "SimpleAdd.tst");
     }
 
     @Test
     public void testStackArithmeticStackTest() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("StackArithmetic").resolve("StackTest");
-        final Path file = getVmFiles(basicTestDir).next();
-
-        VMTranslator.main(new String[]{file.toAbsolutePath().toString(), basicTestDir.resolve("StackTest.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("StackTest.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("StackArithmetic").resolve("StackTest");
+        runTestWithFile(dir, "StackTest.asm", "StackTest.tst");
     }
-    
+
     @Test
     public void testProgramFlowBasicLoop() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("ProgramFlow").resolve("BasicLoop");
-
-        VMTranslator.main(new String[]{basicTestDir.resolve("BasicLoop.vm").toAbsolutePath().toString(), basicTestDir.resolve("BasicLoop.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("BasicLoop.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("ProgramFlow").resolve("BasicLoop");
+        runTestWithFile(dir, "BasicLoop.asm", "BasicLoop.tst");
     }
-    
+
     @Test
     public void testProgramFlowFibonacciSeries() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("ProgramFlow").resolve("FibonacciSeries");
-
-        VMTranslator.main(new String[]{basicTestDir.resolve("FibonacciSeries.vm").toAbsolutePath().toString(), basicTestDir.resolve("FibonacciSeries.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("FibonacciSeries.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("ProgramFlow").resolve("FibonacciSeries");
+        runTestWithFile(dir, "FibonacciSeries.asm", "FibonacciSeries.tst");
     }
-    
+
     @Test
     public void testFunctionCallsFibonacciElement() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("FunctionCalls").resolve("FibonacciElement");
-
-        VMTranslator.main(new String[]{basicTestDir.toAbsolutePath().toString(), basicTestDir.resolve("FibonacciElement.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("FibonacciElement.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("FunctionCalls").resolve("FibonacciElement");
+        runTestWithDir(dir, "FibonacciElement.asm", "FibonacciElement.tst");
     }
-    
+
     @Test
     public void testFunctionCallsSimpleFunction() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("FunctionCalls").resolve("SimpleFunction");
-
-        VMTranslator.main(new String[]{basicTestDir.resolve("SimpleFunction.vm").toAbsolutePath().toString(), basicTestDir.resolve("SimpleFunction.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("SimpleFunction.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("FunctionCalls").resolve("SimpleFunction");
+        runTestWithFile(dir, "SimpleFunction.asm", "SimpleFunction.tst");
     }
-    
+
     @Test
     public void testFunctionCallsStaticsTest() throws IOException, ASMTranslationException {
-        final Path basicTestDir = Resources.RESOURCES_DIR.resolve("FunctionCalls").resolve("StaticsTest");
-
-        VMTranslator.main(new String[]{basicTestDir.toAbsolutePath().toString(), basicTestDir.resolve("StaticsTest.asm").toString()});
-        ASMTester.executeTestScript(
-                basicTestDir.resolve("StaticsTest.tst")
-        );
+        final Path dir = Resources.RESOURCES_DIR.resolve("FunctionCalls").resolve("StaticsTest");
+        runTestWithDir(dir, "StaticsTest.asm", "StaticsTest.tst");
     }
+
 
     public static Iterator<Path> getVmFiles(Path path) throws IOException {
         return Files.list(path)
